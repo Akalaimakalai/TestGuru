d00108dfeff6bba28aba6a0d04fb4ad4b999a45b
Ar4i <jackshaakara@gmail.com>
2019-07-18 19:47:42 +0300

    made seeds greate again!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+categories = Category.create!([
+  { title: "Frontend" },
+  { title: "Backend" },
+  { title: "Other" }
+])
 
-categories = [
-  "Frontend",
-  "Backend",
-  "Other"
-]
+puts "Created: #{categories}"
 
-tests = [
-  [ "How to do frontend", 0, 1 ],
-  [ "How to do frontend 2", 1, 1 ],
-  [ "How to do frontend 3", 2, 1 ],
-  [ "How to do Backend", 0, 2 ],
-  [ "How to do Backend 2", 1, 2 ],
-  [ "How to do Backend 3", 2, 2 ],
-  [ "How to do Other", 0, 3 ],
-  [ "How to do Other 2", 1, 3 ],
-  [ "How to do Other 3", 2, 3 ]
-]
+tests = Test.create!([
+  { title: "How to do frontend", level: 0, category_id: categories[0].id },
+  { title: "How to do frontend 2", level: 1, category_id: categories[0].id },
+  { title: "How to do frontend 3", level: 2, category_id: categories[0].id },
+  { title: "How to do Backend", level: 0, category_id: categories[1].id },
+  { title: "How to do Backend 2", level: 1, category_id: categories[1].id },
+  { title: "How to do Backend 3", level: 2, category_id: categories[1].id },
+  { title: "How to do Other", level: 0, category_id: categories[2].id },
+  { title: "How to do Other 2", level: 1, category_id: categories[2].id },
+  { title: "How to do Other 3", level: 2, category_id: categories[2].id }
+])
 
-users = [
-  ["Griwa", "noob"]
-]
+puts "Created: #{tests}"
 
-results = [
-  [1, 1],
-  [1, 4],
-  [1, 7],
-  [1, 2],
-  [1, 5],
-  [1, 9]
-]
+user = User.create!(name: "GrowaDK", rang: "noob")
 
-categories.each do |cat|
-  Category.create(title: cat)
-end
+puts "Created: #{user}"
 
-tests.each do |t|
-  Test.create(title: t[0], level: t[1], category_id: t[2])
-end
+results = Result.create!([
+  { users_id: user.id, tests_id: tests[0].id },
+  { users_id: user.id, tests_id: tests[1].id },
+  { users_id: user.id, tests_id: tests[4].id },
+  { users_id: user.id, tests_id: tests[3].id },
+  { users_id: user.id, tests_id: tests[7].id }
+])
 
-users.each do |man|
-  User.create(name: man[0], rang: man[1])
-end
-
-results.each do |complete|
-  Result.create(users_id: complete[0], tests_id: complete[1])
-end
+puts "Created: #{results}"
