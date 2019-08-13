46f1e045bf070f811a6233fea8e327026cf87a40
Ar4i <jackshaakara@gmail.com>
2019-08-13 17:15:02 +0300

    solution

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,5 +1,5 @@
 class Result < ApplicationRecord
   belongs_to :user
   belongs_to :test
-  has_one :current_question, class_name: "Question"
+  belongs_to :current_question, class_name: "Question"
 end
