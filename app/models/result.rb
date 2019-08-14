f3fd10a3739e3b920382c3d9db7499d4eaa9feb8
Ar4i <jackshaakara@gmail.com>
2019-08-15 00:03:19 +0300

    last update(for today)

--- app/models/result.rb
+++ app/models/result.rb
@@ -4,13 +4,13 @@ class Result < ApplicationRecord
   belongs_to :current_question, class_name: "Question", optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :before_update_set_next_question
 
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
       self.correct_questions += 1
     end
 
-    self.current_question = next_question
     save!
   end
 
@@ -35,7 +35,7 @@ class Result < ApplicationRecord
     current_question.answers.all_correct
   end
 
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+  def before_update_set_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
