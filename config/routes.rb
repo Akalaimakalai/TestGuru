8db1fc70b3cc45bb4e99758262077f6090c7e967
Ar4i <jackshaakara@gmail.com>
2019-08-28 05:19:16 +0300

    practice devise:controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root 'tests#index'
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout },
+                     controllers: { registrations: 'users/registrations' }
+
 
   resources :tests do
     resources :questions, shallow: true, except: :index  do
