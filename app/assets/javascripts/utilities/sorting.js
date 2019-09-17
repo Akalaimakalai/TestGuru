2f769d671a453f06ec9040d256a49b65a1a69a7c
Ar4i <jackshaakara@gmail.com>
2019-09-17 19:20:54 +0300

    corrections

--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -22,12 +22,12 @@ function sortRowsByTitle() {
 
   if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
     sortedRows.sort(compareRowsAsc);
-    this.querySelector('.octicon-arrow-up').classList.remove('hide')
-    this.querySelector('.octicon-arrow-down').classList.add('hide')
+    this.querySelector('.octicon-arrow-up').classList.remove('hide');
+    this.querySelector('.octicon-arrow-down').classList.add('hide');
   } else {
     sortedRows.sort(compareRowsDesc);
-    this.querySelector('.octicon-arrow-up').classList.add('hide')
-    this.querySelector('.octicon-arrow-down').classList.remove('hide')
+    this.querySelector('.octicon-arrow-up').classList.add('hide');
+    this.querySelector('.octicon-arrow-down').classList.remove('hide');
   }
   
   var sortedTable = document.createElement('table');
