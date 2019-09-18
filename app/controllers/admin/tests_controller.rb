ac71e86aad5a49bbb7f4e4f757d2ea3e0b15efb2
Ar4i <jackshaakara@gmail.com>
2019-09-18 14:05:32 +0300

    practice

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,10 +1,9 @@
 class Admin::TestsController < Admin::BaseController
 
-  before_action :find_test, only: %i[ edit show update destroy ]
+  before_action :set_tests, only: %i[ index update_inline ]
+  before_action :find_test, only: %i[ edit show update update_inline destroy ]
 
-  def index
-    @tests = Test.all
-  end
+  def index; end
 
   def create
     @test = current_user.works.new(test_params)
@@ -30,7 +29,15 @@ class Admin::TestsController < Admin::BaseController
     if @test.update(test_params)
       redirect_to admin_test_path(@test)
     else
-      render :new
+      render :edit
+    end
+  end
+
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
     end
   end
 
@@ -41,6 +48,10 @@ class Admin::TestsController < Admin::BaseController
   end
 
   private
+
+  def set_tests
+    @tests = Test.all
+  end
 
   def find_test
     @test = Test.find(params[:id])
