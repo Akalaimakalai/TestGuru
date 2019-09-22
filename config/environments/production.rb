6d77082a470e5a3eadea2f20f36cea47f92ec152
Ar4i <jackshaakara@gmail.com>
2019-09-22 17:11:31 +0300

    mailer trouble

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'stark-wildwood-45640.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smpt_settings = {
+  config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     user_name:            ENV['SMTP_USERNAME'],
