54ce8095857be710e28c527b064c25d064c1c029
Ar4i <jackshaakara@gmail.com>
2019-10-02 11:34:08 +0300

    correct work with migrations

--- db/migrate/20190926173434_create_badges.rb
+++ db/migrate/20190926173434_create_badges.rb
@@ -2,6 +2,7 @@ class CreateBadges < ActiveRecord::Migration[5.2]
   def change
     create_table :badges do |t|
       t.string :title, null: false
+      t.string :description, null: false
       t.references :user, foreign_key: true
 
       t.timestamps
